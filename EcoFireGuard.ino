7665751b8ee0dfad7d47a4dc50ec98eaea980147
ne0ekspert <rlaaudwk5503@gmail.com>
2023-05-30 17:18:27 +0900

    Update EcoFireGuard.ino

--- EcoFireGuard.ino
+++ EcoFireGuard.ino
@@ -2,21 +2,22 @@
 
 #define SPRINKLER1 2
 #define SPRINKLER2 3
+#define SPRINKLER3 4
+#define SPRINKLER4 5
 
 #define LED1 9
 #define LED2 10
-#define MOTOR 11
+#define LED3 11
+#define LED4 12
+#define MOTOR 13
 
-LiquidCrystal_I2C lcd1(0x26, 16, 2); // 납땜된 LCD
-LiquidCrystal_I2C lcd2(0x27, 16, 2); // 납땜없는 LCD
+LiquidCrystal_I2C lcd1(0x24, 16, 2);
+LiquidCrystal_I2C lcd2(0x25, 16, 2);
+LiquidCrystal_I2C lcd3(0x26, 16, 2);
+LiquidCrystal_I2C lcd4(0x27, 16, 2);
 
-struct floorSettings {
-  byte lcd1=1;
-  byte lcd2=2;
-};
-
-bool led1On = false;
-bool led2On = false;
+bool sprinklerOn[] = { false, false, false, false };
+bool ledOn[] = { false, false, false, false };
 bool motorOn = false;
 
 struct serialInput {
@@ -24,77 +25,83 @@ struct serialInput {
   String value;
 };
 
-struct floorSettings floorInfo;
 struct serialInput serInput;
-
-String stylednum(int num) {
-  switch (num) {
-    case 1:
-      return "1st";
-    case 2:
-      return "2nd";
-    case 3:
-      return "3rd";
-    case 4:
-      return "4th";
-    default:
-      return "<UNK>";
-  }
-}
 
 bool ecoMode = false;
 char str[16];
 
-void countdown(LiquidCrystal_I2C lcd) {
-
-  lcd.setCursor(0, 1);
-
-  for(int i=3;i>0;i--) {
-    sprintf(str, "%d... ", i);
-    lcd.print(str);
-
-    delay(1000);
-  }
-
-  lcd.clear();
-}
-
 void setup() { 
   Serial.begin(115200);
 
-  pinMode(MOTOR, OUTPUT);
+  pinMode(SPRINKLER1, OUTPUT);
+  pinMode(SPRINKLER2, OUTPUT);
+  pinMode(SPRINKLER3, OUTPUT);
+  pinMode(SPRINKLER4, OUTPUT);
+
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
 
-  // 화면 1, 2 초기화
+  pinMode(MOTOR, OUTPUT);
+
+  // 화면 초기화
   lcd1.init();
   lcd2.init();
+  lcd3.init();
+  lcd4.init();
 
-  // 화면 1, 2 LED ON
+  // 화면 LED ON
   lcd1.backlight();
   lcd2.backlight();
+  lcd3.backlight();
+  lcd4.backlight();
 
   lcd1.setCursor(0, 0);
   lcd2.setCursor(0, 0);
+  lcd3.setCursor(0, 0);
+  lcd4.setCursor(0, 0);
 
-  sprintf(str, "Floor set to %d", floorInfo.lcd1);
-  lcd1.print(str);
-  sprintf(str, "Floor set to %d", floorInfo.lcd2);
-  lcd2.print(str);
+  lcd1.print("Floor set 1F");
+  lcd2.print("Floor set 2F");
+  lcd3.print("Floor set 3F");
+  lcd4.print("Floor set 4F");
 
   lcd1.setCursor(0, 1);
   lcd2.setCursor(0, 1);
+  lcd3.setCursor(0, 1);
+  lcd4.setCursor(0, 1);
   
   lcd1.print("Ready");
   lcd2.print("Ready");
+  lcd3.print("Ready");
+  lcd4.print("Ready");
 }
 
 void loop() {
-  if (led1On && !ecoMode) digitalWrite(LED1, HIGH);
+  if (sprinklerOn[0]) digitalWrite(SPRINKLER1, HIGH);
+  else digitalWrite(SPRINKLER1, LOW);
+
+  if (sprinklerOn[1]) digitalWrite(SPRINKLER2, HIGH);
+  else digitalWrite(SPRINKLER2, LOW);
+
+  if (sprinklerOn[2]) digitalWrite(SPRINKLER3, HIGH);
+  else digitalWrite(SPRINKLER3, LOW);
+
+  if (sprinklerOn[3]) digitalWrite(SPRINKLER4, HIGH);
+  else digitalWrite(SPRINKLER4, LOW);
+
+  if (ledOn[0] && !ecoMode) digitalWrite(LED1, HIGH);
   else digitalWrite(LED1, LOW);
 
-  if (led2On && !ecoMode) digitalWrite(LED2, HIGH);
+  if (ledOn[1] && !ecoMode) digitalWrite(LED2, HIGH);
   else digitalWrite(LED2, LOW);
+
+  if (ledOn[2] && !ecoMode) digitalWrite(LED3, HIGH);
+  else digitalWrite(LED3, LOW);
+
+  if (ledOn[4] && !ecoMode) digitalWrite(LED4, HIGH);
+  else digitalWrite(LED4, LOW);
 
   if (motorOn && !ecoMode) digitalWrite(MOTOR, HIGH);
   else digitalWrite(MOTOR, LOW);
@@ -105,56 +112,52 @@ void loop() {
 
     lcd1.setCursor(0, 0);
     lcd2.setCursor(0, 0);
+    lcd3.setCursor(0, 0);
+    lcd4.setCursor(0, 0);
 
     if (serInput.key == "FireAt") { // "FireAt:3"
       lcd1.clear();
       lcd2.clear();
+      lcd3.clear();
+      lcd4.clear();
 
       if (serInput.value != "-1") { // -1은 불이 감지 안 됐을 때
         sprintf(str, "Fire on %dF", serInput.value.toInt());
         lcd1.print(str);
         lcd2.print(str);
+        lcd3.print(str);
+        lcd4.print(str);
 
         lcd1.setCursor(0, 1);
         lcd2.setCursor(0, 1);
+        lcd3.setCursor(0, 1);
+        lcd4.setCursor(0, 1);
 
-        if (serInput.value.toInt() >= floorInfo.lcd1) lcd1.print("Evac Downstairs");
+        if (serInput.value.toInt() >= 1) lcd1.print("Evac Downstairs");
         else lcd1.print("Evac Upstairs");
 
-        if (serInput.value.toInt() >= floorInfo.lcd2) lcd2.print("Evac Downstairs");
+        if (serInput.value.toInt() >= 2) lcd2.print("Evac Downstairs");
         else lcd2.print("Evac Upstairs");
+
+        if (serInput.value.toInt() >= 3) lcd3.print("Evac Downstairs");
+        else lcd3.print("Evac Upstairs");
+
+        if (serInput.value.toInt() >= 4) lcd4.print("Evac Downstairs");
+        else lcd4.print("Evac Upstairs");
       }
     } else if (serInput.key == "EcoMode") { // "EcoMode:1"
       ecoMode = serInput.value == "1";
     } else if (serInput.key == "CtrlIoT") { // "CtrlIoT:M1"
       switch (serInput.value[0]) {
         case '0':
-          led1On = serInput.value[1] == '1';
-          break;
         case '1':
-          led2On = serInput.value[1] == '1';
+        case '2':
+        case '3':
+          ledOn[serInput.value[0] - '0'] = serInput.value[1] == '1';
           break;
         case 'M':
           motorOn = serInput.value[1] == '1';
           break;
-      }
-    } else if (serInput.key == "SetFloor") { // LCD 층 설정 "SetFloor:00"
-      if (serInput.value[0] == '0') {
-        lcd1.clear();
-
-        floorInfo.lcd1 = atoi(&serInput.value[1]);
-        sprintf(str, "Floor set to %d", floorInfo.lcd1);
-        lcd1.print(str);
-
-        countdown(lcd1);
-      } else if (serInput.value[0] == '1') {
-        lcd2.clear();
-
-        floorInfo.lcd2 = atoi(&serInput.value[1]);
-        sprintf(str, "Floor set to %d", floorInfo.lcd2);
-        lcd2.print(str);
-
-        countdown(lcd2);
       }
     }
   }
