dd80cfb8b77bda92fa0a341d524ea12fe25fa23c
ne0ekspert <rlaaudwk5503@gmail.com>
2023-10-24 16:59:51 +0900

    Update docstrings

--- EcoFireGuard.py
+++ EcoFireGuard.py
@@ -1,227 +1,35 @@
-import os, sys
-import crypto
-sys.modules['Crypto'] = crypto
+import sys
 import cv2
-from ultralytics import YOLO
-from gtts import gTTS
-from playsound import playsound
-import time, datetime
-import serial
-import pyrebase
 import threading
-import requests
-import json
 from dotenv import load_dotenv
+from detect import FireDetector
+from delivery import delivery
+from flask_server import WebServer
 
 ## Initial Settings
 load_dotenv(verbose=True)
 
-model = YOLO('best.onnx')
-
 background_image = cv2.imread('bg.jpg')
-
-## Firebase
-## IoT 제어용으로 사용
-config = {
-    "apiKey": os.getenv('FIREBASE_APIKEY') or '',
-    "authDomain": os.getenv('FIREBASE_AUTHDOMAIN') or '',
-    "databaseURL": os.getenv('FIREBASE_DBURL') or '',
-    "storageBucket": os.getenv('FIREBASE_STORAGEBUCKET') or ''
-}
 
 ## Loop
 done = False
 
-detected_people: list[int] = [ 0, 0, 0, 0 ]
-fire: list[bool] = [ False, False, False, False ]
-detect_ready: list[bool] = [ False, False, False, False ]
-result_frames = [ None, None, None, None ]
+# 카메라 / 화재 인식 객체
+camera0 = FireDetector(3, 0)
+camera1 = FireDetector(2, 3)
+camera2 = FireDetector(1, 2)
+camera3 = FireDetector(0, 1)
 
-time.sleep(1)
+webviewer = WebServer(camera0, camera1, camera2, camera3)
 
-def detect(index, changed_index) -> None:
-    cap = cv2.VideoCapture(index)
+# 화재 인식 스레드
+t0 = threading.Thread(target=camera0.detect, args=(3, 0))
+t1 = threading.Thread(target=camera1.detect, args=(2, 3))
+t2 = threading.Thread(target=camera2.detect, args=(1, 2))
+t3 = threading.Thread(target=camera3.detect, args=(0, 1))
 
-    while not done:
-        success, frame = cap.read()
-
-        if success:
-            detect_start_time = time.time()
-            try:
-                results = model.predict(frame, half=True, device='cpu', verbose=False)
-            except:
-                continue
-            result_frame = frame
-
-            detected_objects = {
-                'fire': 0,
-                'person': 0
-            }
-
-            for result in results:
-                boxes = result.boxes
-
-                for box in boxes:
-                    color: tuple[int, int, int] = (0, 255, 0) if box.cls == 0 else (0, 0, 255)
-                    # box.xyxy, box.cls는 tensor
-                    position = list(map(int, box.xyxy.tolist()[0])) # 출력값은 float인데 int로 변환
-                    # Class ID
-                    # 0: Fire
-                    # 1: Person
-                    detect_class_id = int(box.cls.tolist()[0])
-                    detect_class_label: str = 'person' if detect_class_id == 1 else 'fire'
-
-                    if position[0] > 400:
-                        continue
-
-                    if detect_class_id == 0: # On fire detected
-                        detected_objects['fire'] += 1
-
-                    if detect_class_id == 1: # On person detected
-                        detected_objects['person'] += 1
-
-                    result_frame = cv2.rectangle(result_frame, (position[0], position[1]), (position[2], position[3]), color, 2)
-
-                    result_frame = cv2.putText(result_frame, detect_class_label, (position[0], position[1]), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255 ,255), 1, cv2.LINE_AA)
-
-            result_frame = cv2.putText(result_frame, f"{changed_index+1}Floor", (0, 50), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 255, 0), 2, cv2.LINE_AA)
-
-            detect_ready[changed_index] = True
-            detected_people[changed_index] = detected_objects['person']
-            fire[changed_index] = True if detected_objects['fire'] > 0 else False
-            result_frames[-changed_index-1] = result_frame
-
-            #`MNprint(f"{index}: Detection Time: {time.time()-detect_start_time}")
-    
-    cap.release()
-
-def fetch(url, message):
-    def webhookData(message: str):
-        return json.dumps({
-            "username": "Fire Alert",
-            "content": message,
-            "text": message
-        })
-    
-    res = requests.post(url, webhookData(message), headers={
-        'Content-Type': 'application/json'
-    })
-    
-    if res.ok:
-        print("Webhook sent")
-    else:
-        print(res.status_code)
-
-def delivery() -> None:
-    # 시리얼 포트 설정
-    ser = serial.Serial(port=os.getenv('SERIAL_PORT'), baudrate=9600, timeout=0)
-    datetime_object = datetime.datetime.fromtimestamp(time.time())
-
-    WEBHOOK_URL = os.getenv('WEBHOOK_URL') or ''
-    WEBHOOK_REFRESH_DELAY = float(os.getenv('WEBHOOK_REFRESH_DELAY') or 5.0)
-    OPENWEATHERMAP_API_KEY = os.getenv('OPENWEATHERMAP_API_KEY')
-
-    LCD_REFRESH_DELAY = float(os.getenv('LCD_REFRESH_DELAY') or 5.0)
-    ALERT_REFRESH_DELAY = float(os.getenv('ALERT_REFRESH_DELAY') or 5.0)
-    WEATHER_REFRESH_DELAY = float(os.getenv('WEATHER_REFRESH_DELAY') or 600.0)
-
-    last_sent_timestamp = time.time() - LCD_REFRESH_DELAY
-    last_alert_timestamp = time.time() - ALERT_REFRESH_DELAY
-    last_weather_timestamp = time.time() - WEATHER_REFRESH_DELAY
-
-    ser.write(f"Time:{int((datetime_object - datetime.datetime(1970, 1, 1)).total_seconds())}\n".encode())
-
-    folder = 'res'
-
-    iot_status: dict[str, str] = {
-        'LED1': 'false',
-        'LED2': 'false',
-        'LED3': 'false',
-        'LED4': 'false'
-    }
-    def iot_stream_handler(message):
-        ser.flush()
-        path = message['path'][1:]
-        if len(path) == 0:
-            pass
-        else:
-            iot_status[path] = message['data']
-
-        ser.write(f"CtrlIoT:".encode())
-        print(f"[{time.time()}] CtrlIoT")
-        ser.write(''.join('1' if iot_status[f'LED{i+1}'] == 'true' else '0' for i in range(4)).encode())
-        print(f"[{time.time()}] {'1' if iot_status[f'LED{i+1}'] == 'true' else '0'}")
-        ser.write('\n'.encode())
-        
-        print(iot_status)
-        print(message)
-
-    firebase = pyrebase.initialize_app(config)
-    db = firebase.database()
-    iot_stream = db.child('light').stream(iot_stream_handler)
-
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    while not done:
-        fire_floor = []
-        for i in range(len(fire)):
-            if fire[i]:
-                fire_floor.append(str(i+1))
-
-        if sum(detected_people) == 0:
-            ser.write('EcoMode:1\n'.encode())
-            print(f"[{time.time()}] EcoMode:1")
-        else:
-            ser.write('EcoMode:0\n'.encode())
-            print(f"[{time.time()}] EcoMode:0")
-
-        if len(fire_floor) > 0:
-            ser.write(f"FireAt:{','.join(fire_floor)}\n".encode())
-
-            message = f"Fire detected on floor {', '.join(fire_floor)}"
-            if last_sent_timestamp + WEBHOOK_REFRESH_DELAY <= time.time():
-                fetch(WEBHOOK_URL, message)
-                last_sent_timestamp = time.time()
-
-            if last_alert_timestamp + ALERT_REFRESH_DELAY <= time.time():
-                text = f"{'층, '.join(fire_floor)}층에 화재가 감지되었습니다."
-                filename = f"fireat_{''.join(fire_floor)}.mp3"
-                filepath = os.path.join(folder, filename)
-
-                if not os.path.exists(filepath):
-                    tts = gTTS(text=text, lang='ko')
-                    tts.save(filepath)
-                    print(f"WAV file '{filename}' generated successfully.")
-                else:
-                    print(f"WAV file '{filename}' already exists.")
-                
-                playsound(filepath, block=False)
-                last_alert_timestamp = time.time()
-
-            if last_weather_timestamp + WEATHER_REFRESH_DELAY <= time.time():
-                res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={OPENWEATHERMAP_LAT}&lon={OPENWEATHERMAP_LONG}&appid={OPENWEATHERMAP_API_KEY}&units=metric")
-                weather = res.json()
-                print(weather)
-                ser.write(f"Weather:{weather['weather'][0]['main']}\n".encode())
-                ser.write(f"Temp:{weather['main']['temp']}\n".encode())
-                last_weather_timestamp = time.time()
-
-            print(f"[{time.time()}] FireAt:{','.join(fire_floor)}")
-        else:
-            ser.write('FireAt:0\n'.encode())
-            print(f"[{time.time()}] FireAt:0")
-    
-        time.sleep(LCD_REFRESH_DELAY)
-
-    ser.close()
-
-t0 = threading.Thread(target=detect, args=(3, 0))
-t1 = threading.Thread(target=detect, args=(2, 3))
-t2 = threading.Thread(target=detect, args=(1, 2))
-t3 = threading.Thread(target=detect, args=(0, 1))
-
-delivery_thread = threading.Thread(target=delivery)
+delivery_thread = threading.Thread(target=delivery, args=(camera0, camera1, camera2, camera3))
+viewer_thread = threading.Thread(target=webviewer.run)
 
 t0.daemon = True
 t1.daemon = True
@@ -230,6 +38,7 @@ t3.daemon = True
 
 delivery_thread.daemon = True
 
+# 스레드 시작
 t0.start()
 t1.start()
 t2.start()
@@ -237,13 +46,15 @@ t3.start()
 
 delivery_thread.start()
 
+# 창 생성
 cv2.namedWindow("Object Detection", cv2.WINDOW_NORMAL)
 
 while not done:
     try:
-        if all(detect_ready):
+        if all([camera0.isReady, camera1.isReady, camera2.isReady, camera3.isReady]):
             visual_frame = background_image
-            result_frame = cv2.vconcat([cv2.hconcat(result_frames[:2]), cv2.hconcat(result_frames[2:])])
+            result_frame = cv2.vconcat([cv2.hconcat(camera0.resultFrame, camera1.resultFrame),
+                                             cv2.hconcat(camera2.resultFrame, camera3.resultFrame)])
             visual_frame[469:1469, 690:2023] = cv2.resize(result_frame, (1333, 1000))
             cv2.imshow("Object Detection", visual_frame)
 
@@ -251,6 +62,11 @@ while not done:
             done = True
     except KeyboardInterrupt:
         done = True
+
+camera0.release_camera()
+camera1.release_camera()
+camera2.release_camera()
+camera3.release_camera()
 
 cv2.destroyAllWindows()
 sys.exit(0)
---
+++ detect.py
@@ -0,0 +1,104 @@
+import cv2
+import time
+from ultralytics import YOLO
+
+# 화재 인식 모델 불러오기
+model = YOLO('best.onnx')
+
+class FireDetector:
+    """
+    화재 감지 클래스
+
+    Args:
+        index (int): 실제 카메라의 ID
+        changed_index (int): 카메라와 층 수가 맞지 않을 경우 변경할 번호
+    
+    Attributes:
+        index (int): 카메라의 ID
+        changed_index (int): 바꿀 ID
+
+        done (bool): 동작 완료 플래그
+        detect_ready (bool): 인식 준비 완료 플래그
+        cap (VideoCapture): 카메라 객체
+    """
+    def __init__(self, index, changed_index):
+        self.index = index
+        self.changed_index = changed_index
+
+        self.done = False
+        self.detect_ready = False
+        self.cap = cv2.VideoCapture(index)
+
+    def detect(self) -> None:
+        """
+        화재 인식 스레드 함수
+        """
+        while not self.done:
+            success, frame = self.cap.read()
+
+            if success:
+                detect_start_time = time.time()
+                try:
+                    # 화재 감지 모델로 화재 감지
+                    results = model.predict(frame, half=True, device='cpu', verbose=False)
+                except:
+                    continue
+                result_frame = frame
+
+                detected_objects = {
+                    'fire': 0,
+                    'person': 0
+                }
+
+                for result in results:
+                    boxes = result.boxes
+
+                    for box in boxes:
+                        color: tuple[int, int, int] = (0, 255, 0) if box.cls == 0 else (0, 0, 255)
+
+                        position = list(map(int, box.xyxy.tolist()[0])) # 출력값은 float인데 int로 변환
+                        # Class ID
+                        # 0: Fire
+                        # 1: Person
+                        detect_class_id = int(box.cls.tolist()[0])
+                        detect_class_label: str = 'person' if detect_class_id == 1 else 'fire'
+
+                        if position[0] > 400:
+                            continue
+
+                        if detect_class_id == 0: # On fire detected
+                            detected_objects['fire'] += 1
+
+                        if detect_class_id == 1: # On person detected
+                            detected_objects['person'] += 1
+
+                        result_frame = cv2.rectangle(result_frame, (position[0], position[1]), (position[2], position[3]), color, 2)
+
+                        result_frame = cv2.putText(result_frame, detect_class_label, (position[0], position[1]), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255 ,255), 1, cv2.LINE_AA)
+
+                result_frame = cv2.putText(result_frame, f"{self.changed_index+1}Floor", (0, 50), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 255, 0), 2, cv2.LINE_AA)
+
+                self.detect_ready = True
+                self.detected_people = detected_objects['person']
+                self.fire = True if detected_objects['fire'] > 0 else False
+                self.result_frame = result_frame
+
+    def release_camera(self):
+        self.done = True
+        self.cap.release()
+
+    @property
+    def isReady(self):
+        return self.detect_ready
+
+    @property
+    def personCount(self):
+        return self.detected_people
+    
+    @property
+    def fireDetectedFloor(self) -> int:
+        return self.changed_index if self.fire else 0
+
+    @property
+    def resultFrame(self):
+        return self.result_frame
